3973d8d52e9ac9fa0e94e2435a79addf26410fe8
Muhamad Syafi'i <muhamad.syafii@cxrus.com>
2019-12-02 17:35:58 +0700

    adding adapter

--- app/src/main/java/dev/syafii/makanancirebon/adapter/MakananAdapter.java
+++ app/src/main/java/dev/syafii/makanancirebon/adapter/MakananAdapter.java
@@ -1,16 +1,36 @@
 package dev.syafii.makanancirebon.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
+import dev.syafii.makanancirebon.data.model.Makanan;
+
 public class MakananAdapter extends RecyclerView.Adapter<MakananAdapter.ListViewHolder> {
 
+    public interface onItemClickCallback{
+        void onItemClicked(Makanan data);
+    }
+    private onItemClickCallback onItemClickCallback;
+    private ArrayList<Makanan> listMakan;
+
+    public MakananAdapter(ArrayList<Makanan> listMakan){
+        this.listMakan = listMakan;
+        notifyDataSetChanged();
+    }
+
+    public void setOnItemClickCallback(onItemClickCallback onItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback;
+    }
     @NonNull
     @Override
     public MakananAdapter.ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        View view = LayoutInflater.from(parent.getContext()).inflate()
         return null;
     }
 
